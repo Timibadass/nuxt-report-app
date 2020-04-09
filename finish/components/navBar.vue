5d6b9775dff4954b6c140ef4a5108398e74b792f
Timibadass <oblacishere@gmail.com>
2020-04-09 12:52:18 +0100

    initial commit

---
+++ finish/components/navBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="header">
+    <div class="logo">
+      <nuxt-link to="/">
+        <Logo />
+      </nuxt-link>
+    </div>
+
+    <nav class="nav">
+      <div class="nav__link">
+        <nuxt-link to="/">Home</nuxt-link>
+      </div>
+      <div class="nav__link">
+        <nuxt-link to="/About">About</nuxt-link>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+import Logo from "@/components/Logo";
+export default {
+  name: "nav-bar",
+  components: {
+    Logo
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ start/store/index.js
@@ -0,0 +1,13 @@
+export const state = () => ({})
+
+export const getters = {
+
+}
+
+export const mutations = {
+
+}
+
+export const actions = {
+
+}
